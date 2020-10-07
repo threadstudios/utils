1ff0f91a3f80de8c89f875f1ffbd51364c0d4500
Paul.Westerdale <paul@westerdale.me>
2020-10-07 22:05:34 +0100

    update and fix migrations

--- packages/migrations/index.js
+++ packages/migrations/index.js
@@ -1,21 +1,30 @@
 #!/usr/bin/env node
 
-require("dotenv").config();
-const fs = require("fs-extra");
-const program = require("commander");
-const commands = require("./src/commands");
+require('dotenv').config()
+const fs = require('fs-extra')
+const program = require('commander')
+const commands = require('./src/commands')
 
-const template = fs.readFileSync(`${__dirname}/template.js`, "utf-8");
-const appPath = process.cwd();
+const template = fs.readFileSync(`${__dirname}/template.js`, 'utf-8')
+const appPath = process.cwd()
 
-program.command("make <name>").action(name => {
-  fs.ensureDirSync(`${appPath}/migrations`);
-  const filename = [Date.now(), name].join(".");
-  fs.writeFileSync(`${appPath}/migrations/${filename}.js`, template);
-});
+program.command('make <name>').action(name => {
+  fs.ensureDirSync(`${appPath}/migrations`)
+  const filename = [Date.now(), name].join('.')
+  fs.writeFileSync(`${appPath}/migrations/${filename}.js`, template)
+})
 
-program.command("run").action(commands.run);
-program.command("down").action(commands.down);
-program.command("teardown").action(commands.teardown);
+program.command('run').action(async () => {
+  await commands.run()
+  process.exit()
+})
+program.command('down').action(async () => {
+  await commands.down()
+  process.exit()
+})
+program.command('teardown').action(async () => {
+  await commands.teardown()
+  process.exit()
+})
 
-program.parse(process.argv);
+program.parse(process.argv)
--- packages/migrations/src/commands.js
+++ packages/migrations/src/commands.js
@@ -1,69 +1,84 @@
-const chalk = require("chalk");
-const redis = require("@threadws/redis");
-const logger = require("@threadws/logger");
-const migration = require("./migrations");
+const chalk = require('chalk')
+const redis = require('@threadws/redis')
+const logger = require('@threadws/logger')
+const migration = require('./migrations')
 
 module.exports.run = async () => {
-  const migratedTS = await redis.get("migrations:ts");
-  let currentTs = migratedTS;
+  const migratedTS = await redis.get('migrations:ts')
+  let currentTs = migratedTS
   const files = migration.getMigrations().filter(file => {
-    return file && (migratedTS === null || migratedTS < file.ts);
-  });
+    return file && (migratedTS === null || migratedTS < file.ts)
+  })
   if (files.length) {
     // eslint-disable-next-line no-restricted-syntax
     for (const file of files) {
-      logger.info(chalk.green(`running migration ${file.name}`));
+      logger.info(chalk.green(`running migration ${file.name}`))
       // eslint-disable-next-line import/no-dynamic-require, global-require
-      const migrationFile = require(`${file.path}`);
+      const migrationFile = require(`${file.path}`)
       // eslint-disable-next-line no-await-in-loop
-      await migrationFile.up();
-      currentTs = file.ts;
+      try {
+        await migrationFile.up()
+      } catch (e) {
+        return Promise.reject(e)
+      }
+      currentTs = file.ts
+      await redis.set('migrations:ts', currentTs)
     }
-    await redis.set("migrations:ts", currentTs);
   } else {
-    logger.info(chalk.green(`No migrations found to run`));
+    logger.info(chalk.green(`No migrations found to run`))
   }
-  redis.disconnect();
-};
+  redis.disconnect()
+  return Promise.resolve()
+}
 
 module.exports.down = async () => {
-  const currentTs = await redis.get("migrations:ts");
-  const files = migration.getMigrations();
-  const currentIndex = files.findIndex(file => file.ts === currentTs);
+  const currentTs = await redis.get('migrations:ts')
+  const files = migration.getMigrations()
+  const currentIndex = files.findIndex(file => file.ts === currentTs)
   // current migration exists - run down
   if (files[currentIndex]) {
-    const file = files[currentIndex];
+    const file = files[currentIndex]
     // eslint-disable-next-line import/no-dynamic-require, global-require
-    const migrationFile = require(`${file.path}`);
-    logger.info(chalk.green(`running down ${file.name}`));
-    await migrationFile.down();
-    const newTs = files[currentIndex - 1] ? files[currentIndex - 1].ts : 0;
-    await redis.set("migrations:ts", newTs);
+    const migrationFile = require(`${file.path}`)
+    logger.info(chalk.green(`running down ${file.name}`))
+    try {
+      await migrationFile.down()
+    } catch (e) {
+      return Promise.reject(e)
+    }
+    const newTs = files[currentIndex - 1] ? files[currentIndex - 1].ts : 0
+    await redis.set('migrations:ts', newTs)
   } else {
-    logger.error(chalk.red(`no migration found matching ${currentTs}`));
+    logger.error(chalk.red(`no migration found matching ${currentTs}`))
   }
-  redis.disconnect();
-};
+  redis.disconnect()
+  return Promise.resolve()
+}
 
 module.exports.teardown = async () => {
-  const currentTs = await redis.get("migrations:ts");
+  const currentTs = await redis.get('migrations:ts')
   const files = migration.getMigrations().filter(file => {
-    return file && currentTs >= file.ts;
-  });
+    return file && currentTs >= file.ts
+  })
   if (files.length) {
     // eslint-disable-next-line no-restricted-syntax
     for (const file of files) {
-      logger.info(chalk.green(`running down migration ${file.name}`));
+      logger.info(chalk.green(`running down migration ${file.name}`))
       // eslint-disable-next-line import/no-dynamic-require, global-require
-      const migrationFile = require(`${file.path}`);
+      const migrationFile = require(`${file.path}`)
       // eslint-disable-next-line no-await-in-loop
-      await migrationFile.down();
+      try {
+        await migrationFile.down()
+      } catch (e) {
+        return Promise.reject(e)
+      }
       // eslint-disable-next-line no-await-in-loop
-      await redis.set("migrations:ts", file.ts);
+      await redis.set('migrations:ts', file.ts)
     }
-    await redis.set("migrations:ts", 0);
+    await redis.set('migrations:ts', 0)
   } else {
-    logger.info(chalk.green(`No migrations found to run`));
+    logger.info(chalk.green(`No migrations found to run`))
   }
-  redis.disconnect();
-};
+  redis.disconnect()
+  return Promise.resolve()
+}
