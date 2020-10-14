7d6205fd393082fb06bdb1e4424f7d3853f5b3b9
Paul.Westerdale <paul@westerdale.me>
2020-10-14 09:29:33 +0100

    update redis usage

--- packages/redis/index.js
+++ packages/redis/index.js
@@ -1,6 +1,22 @@
-const Redis = require("ioredis");
+const Redis = require('ioredis')
+const logger = require('@threadws/logger')
 
-const redisClient = new Redis(process.env.REDIS_CONNECTION_STRING);
-redisClient.select(process.env.REDIS_INDEX || 0);
+const isCluster = process.env.REDIS_MODE === 'cluster'
 
-module.exports = redisClient;
+let redis
+
+if (isCluster) {
+  redis = new Redis.Cluster([process.env.REDIS_CONNECTION_STRING], {
+    scaleReads: 'slave',
+  })
+} else {
+  redis = new Redis(process.env.REDIS_CONNECTION_STRING)
+}
+
+redis.select(process.env.REDIS_INDEX || 0)
+
+redis.on('error', err => {
+  logger.error('REDIS: FAILED', { detail: { ...err } })
+})
+
+module.exports = redis
